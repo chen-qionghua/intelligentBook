9cad647b6a0fd0ae4e1c4560498f27b34e47f38f
npc <1169121469@qq.com>
2023-01-16 11:47:07 +0800

    feature:初始化welcome页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,15 @@
+import { RouteRecordRaw } from 'vue-router'
+import { First } from '../components/welcom/first';
+import { Sencond } from '../components/welcom/sencond';
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from '../views/Welcome';
 
-export const routes = [
+export const routes:RouteRecordRaw[] = [
   {path:'/',component:Foo},
   {path:'/about',component:Bar},
+  {path:'/welcome',component:Welcome,children:[
+    {path:'1',component:First},
+    {path:'2',component:Sencond}
+  ]}
 ]
