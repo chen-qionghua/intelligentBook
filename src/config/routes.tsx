8b9019f4db754604454703f48bd0d6b5f0a9453a
npc <1169121469@qq.com>
2023-01-16 22:08:00 +0800

    fixed routes

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,14 +1,12 @@
 import { RouteRecordRaw } from 'vue-router'
 import { First } from '../components/welcome/Frist';
 import { Sencond } from '../components/welcome/Sencond';
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { Welcome } from '../views/Welcome';
 
 export const routes:RouteRecordRaw[] = [
-  {path:'/',component:Foo},
-  {path:'/about',component:Bar},
+  {path:'/',redirect:'/welcome'},
   {path:'/welcome',component:Welcome,children:[
+    {path:'',redirect:'/welcome/1'},
     {path:'1',component:First},
     {path:'2',component:Sencond}
   ]}
