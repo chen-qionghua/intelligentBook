0e88d924d9d1f21036d409561cb138361f423e71
npc <1169121469@qq.com>
2023-01-16 11:26:37 +0800

    refactor:重构main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { createApp } from 'vue'
 import { App } from './App'
-import { Bar } from './views/Bar'
-import { Foo } from './views/Foo'
-import {createRouter, createWebHashHistory} from 'vue-router'
-
-const routes = [
-  {path:'/',component:Foo},
-  {path:'/about',component:Bar},
-]
-const router = createRouter({
-  history:createWebHashHistory(),
-  routes
-})
+import { history } from './shared/history'
+import { createRouter } from 'vue-router'
+import { routes } from './config/routes'
+const router = createRouter({history,routes})
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
